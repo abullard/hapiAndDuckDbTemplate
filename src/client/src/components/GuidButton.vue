45610d88c94510bc9fcdcb9e5eae9bee2e96eb89
root <root@WINTERMUTE.localdomain>
2025-06-06 14:39:33 -0700

    removed unused, and added guid list to Vue. Hardcoded for now

---
+++ src/client/src/components/GuidButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button class="guid-btn">Generate New Database Entry</button>
+    </div>
+</template>
+
+<style>
+.guid-btn:hover {
+    background-color: #002ead;
+}
+
+.guid-btn {
+    background-color: #0a0a23;
+    color: #fff;
+    border: none;
+    border-radius: 10px;
+    padding: 15px;
+    min-height: 30px;
+    min-width: 120px;
+}
+</style>
---
+++ src/client/src/components/GuidList.vue
@@ -0,0 +1,32 @@
+<script setup>
+const items = [
+    { id: '7d8dc10d-bfb4-4b0e-8252-e993e69b6902', count: '1' },
+    { id: '98ce3e10-22e7-4e34-bd5a-d6a72c4aed6f', count: '2' },
+    { id: '98ce3e10-22e7-4e34-bd5a-d6a72c4aed6f', count: '3' },
+    { id: '98ce3e10-22e7-4e34-bd5a-d6a72c4aed6f', count: '4' },
+    { id: '98ce3e10-22e7-4e34-bd5a-d6a72c4aed6f', count: '5' },
+    { id: '98ce3e10-22e7-4e34-bd5a-d6a72c4aed6f', count: '6' },
+    { id: '98ce3e10-22e7-4e34-bd5a-d6a72c4aed6f', count: '7' },
+    { id: '98ce3e10-22e7-4e34-bd5a-d6a72c4aed6f', count: '8' },
+    { id: '98ce3e10-22e7-4e34-bd5a-d6a72c4aed6f', count: '9' },
+    { id: '98ce3e10-22e7-4e34-bd5a-d6a72c4aed6f', count: '10' },
+    { id: '98ce3e10-22e7-4e34-bd5a-d6a72c4aed6f', count: '11' },
+    { id: '98ce3e10-22e7-4e34-bd5a-d6a72c4aed6f', count: '12' },
+    { id: '98ce3e10-22e7-4e34-bd5a-d6a72c4aed6f', count: '13' },
+]
+</script>
+
+<template>
+    <div class="database-items" v-for="item of items">
+        <p>{{ item.count }} - {{ item.id }}</p>
+    </div>
+</template>
+
+<style scoped>
+.database-items {
+    outline: solid 1px gray;
+    border-radius: 4px;
+    padding: 0.5rem;
+    margin: 1rem;
+}
+</style>
